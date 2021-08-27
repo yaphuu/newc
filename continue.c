d2753584dcca42b02680cdcb1a4c7d8e8aa1ba8b
yaphuu <zhangkai_1988@163.com>
2021-08-27 20:38:25 +0800

    Add files via upload

---
+++ continue.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+    int count = 0;
+    char ch;
+
+    while (count < 10)
+    {
+        ch = getchar();
+        putchar(ch);
+        count++;
+        
+    }
+
+    return 0;
+}
